3a323a1d79c22521da33b365d349e364e2089d5d
Conor Kelly <conormpkelly@gmail.com>
2019-12-04 01:33:59 +0000

    Tests all passing

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 // Core node modules
 const { unzipSync } = require("zlib");
+
+const path = require("path");
 const fs = require("fs");
 
-
 // Clean each resulant chunk of unformatted json
-function parseTarball(inputFilePath) {
-  // Get the buffer from the file
-  const buffer = fs.readFileSync(inputFilePath);
+module.exports.parseTarball = inputFilePath => {
+  if (!inputFilePath || typeof inputFilePath !== "string") {
+    throw new Error(`InvalidArgument: No file path supplied`);
+  }
 
-  return unzipSync(buffer)
-    .toString("utf-8")
-    .split("\0ustar") // Separate into headers & files
-    .slice(2) // First 2 entries are headers
-    .map(rawJsonString => getFormattedJSON(rawJsonString));
+  const filename = path.basename(inputFilePath);
+
+  try {
+    // Get the buffer from the file
+    const buffer = fs.readFileSync(inputFilePath);
+
+    return unzipSync(buffer)
+      .toString("utf-8")
+      .split("\0ustar") // Separate into headers & files
+      .slice(2) // First 2 entries are headers
+      .map(rawJsonString => getFormattedJSON(rawJsonString));
+  } catch (err) {
+    handleError(err, filename);
+  }
+};
+
+function handleError(err, filename) {
+  if (
+    err.message.includes("Unexpected end of JSON input") ||
+    err.message.includes("Unexpected token")
+  ) {
+    throw new Error(`InvalidJSON: Failed to parse '${filename}'`);
+  } else if (err.message.includes("no such file or directory")) {
+    throw new Error(`FileNotFound: '${filename}'`);
+  } else if (err.message.includes("unexpected end of file")) {
+    throw new Error(`EmptyFile: '${filename}'`);
+  } else if (err.message.includes("incorrect header check")) {
+    throw new Error(`InvalidFileFormat: '${filename}'`);
+  } else {
+    throw err;
+  }
 }
 
 function getFormattedJSON(rawJsonString) {
@@ -36,6 +64,3 @@ function firstIndexOf(chars, str) {
 function lastIndexOf(chars, str) {
   return Math.max(...chars.map(c => str.lastIndexOf(c)));
 }
-
-const jsonArray = parseTarball("./tarballs/array-1-item.tar.gz");
-console.log(JSON.stringify(jsonArray, null, 2));
---
+++ tests/app.spec.js
@@ -0,0 +1,128 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const tarball2JS = require("../app");
+
+describe("Valid JSON Arrays", () => {
+  describe("given a tarball containing 1 valid jsonArray file", () => {
+    it("should return an array", () => {
+      const resultData = tarball2JS.parseTarball(
+        "./tarballs/array-1-item.tar.gz"
+      );
+
+      expect(Array.isArray(resultData)).to.be.true;
+    });
+  });
+
+  describe("given a tarball containing 1 valid jsonArray file", () => {
+    it("should return an array of length 1", () => {
+      const resultData = tarball2JS.parseTarball(
+        "./tarballs/array-1-item.tar.gz"
+      );
+      expect(resultData.length === 1).to.be.true;
+    });
+  });
+
+  describe("given a tarball containing 3 valid jsonArray files", () => {
+    it("should return an array of length 3", () => {
+      const resultData = tarball2JS.parseTarball(
+        "./tarballs/array-3-items.tar.gz"
+      );
+      expect(resultData.length === 3).to.be.true;
+    });
+  });
+
+  describe("given a tarball containing no files", () => {
+    it("should return an empty array", () => {
+      const resultData = tarball2JS.parseTarball("./tarballs/no-files.tar.gz");
+      expect(resultData.length === 0).to.be.true;
+    });
+  });
+});
+
+describe("Valid JSON Object", () => {
+  describe("given a tarball containing 1 valid json Object file", () => {
+    it("should return an array", () => {
+      const resultData = tarball2JS.parseTarball("./tarballs/valid-obj.tar.gz");
+
+      expect(Array.isArray(resultData)).to.be.true;
+    });
+  });
+
+  describe("given a tarball containing 1 valid json Object file", () => {
+    it("should return an array of length 1", () => {
+      const resultData = tarball2JS.parseTarball(
+        "./tarballs/array-1-item.tar.gz"
+      );
+      expect(resultData.length === 1).to.be.true;
+    });
+  });
+
+  describe("given a tarball containing 1 empty json Object file", () => {
+    it("should return an array of length 1", () => {
+      const resultData = tarball2JS.parseTarball("./tarballs/empty-obj.tar.gz");
+      expect(resultData.length === 1).to.be.true;
+    });
+  });
+});
+
+describe("Invalid JSON Types", () => {
+  describe("given a tarball containing 1 invalid jsonArray file", () => {
+    it("should throw an InvalidJSON error", () => {
+      expect(
+        tarball2JS.parseTarball.bind(tarball2JS, "./tarballs/invalid-array.tar.gz")
+      ).to.throw("InvalidJSON: Failed to parse 'invalid-array.tar.gz'");
+    });
+  });
+
+  describe("given a tarball containing 1 invalid json Object file", () => {
+    it("should throw an InvalidJSON error", () => {
+      expect(
+        tarball2JS.parseTarball.bind(tarball2JS, "./tarballs/invalid-obj.tar.gz")
+      ).to.throw("InvalidJSON: Failed to parse 'invalid-obj.tar.gz'");
+    });
+  });
+
+});
+
+describe("Invalid Path supplied", () => {
+  describe("given a file that doesn't exist", () => {
+    it("should throw a FileNotFound error", () => {
+      expect(
+        tarball2JS.parseTarball.bind(tarball2JS, "./tarballs/invalid-filename.tar.gz")
+      ).to.throw("FileNotFound: 'invalid-filename.tar.gz'");
+    });
+  });
+
+  describe("given empty filePath argument", () => {
+    it("should throw an InvalidArgument error", () => {
+      expect(
+        tarball2JS.parseTarball.bind(tarball2JS, "")
+      ).to.throw("InvalidArgument: No file path supplied");
+    });
+  });
+
+  describe("given NO filePath argument", () => {
+    it("should throw an InvalidArgument error", () => {
+      expect(
+        tarball2JS.parseTarball.bind(tarball2JS, null)
+      ).to.throw("InvalidArgument: No file path supplied");
+    });
+  });
+
+  describe("given empty file", () => {
+    it("should throw an EmptyFile error", () => {
+      expect(
+        tarball2JS.parseTarball.bind(tarball2JS, "./tarballs/empty.tar.gz")
+      ).to.throw("EmptyFile: 'empty.tar.gz'");
+    });
+  });
+
+  describe("given wrong file type", () => {
+    it("should throw an InvalidFileFormat error", () => {
+      expect(
+        tarball2JS.parseTarball.bind(tarball2JS, "./tarballs/not-a-tar.gz")
+      ).to.throw("InvalidFileFormat: 'not-a-tar.gz'");
+    });
+  });
+});
