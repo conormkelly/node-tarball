dfc09e3e06ab0297cb149b6678670b8f34df8f4f
Conor Kelly <conormpkelly@gmail.com>
2019-12-05 23:14:45 +0000

    format: removed magic str/nums

--- main.js
+++ main.js
@@ -6,19 +6,23 @@ const fs = require('fs');
 // Local files
 const { determineError } = require('./errorFactory');
 const { getFormattedJSON } = require('./jsonUtils');
+const {
+  STRING_ENCODING,
+  TARFILE_SEPARATOR,
+  HEADER_DATA
+} = require('./constants');
 
 module.exports.extractJSONArray = (inputFilePath, onComplete) => {
   let filename;
 
   try {
     filename = path.basename(inputFilePath);
-    // Get the buffer from the file
     const buffer = fs.readFileSync(inputFilePath);
 
     const results = unzipSync(buffer)
-      .toString('utf-8')
-      .split('\0ustar') // Separate into headers & files
-      .slice(2) // First 2 entries are headers
+      .toString(STRING_ENCODING)
+      .split(TARFILE_SEPARATOR) // Separate into headers & files
+      .slice(HEADER_DATA) // First 2 entries are headers
       .map(rawJsonString => getFormattedJSON(rawJsonString));
 
     onComplete(null, results);
